29a8ac169535e084f4b154dc872cc7346db6181e
Luckey <luckeysherpa07@gmail.com>
2025-10-24 01:09:27 +0200

    added product card

--- src/app/pages/products-list/product-card/product-card.ts
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../models/products.model';
+import { PrimaryButton } from "../../../components/primary-button/primary-button";
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [PrimaryButton],
   template: `
-    <p>
-      product-card works!
-    </p>
+    <div class="bg-white shadow-md rounded-xl p-6 flex flex-col gap-6 relative">
+      <div class="mx-auto">
+      <img [src]="product().image" class="w-[200px] h-[100px] object-contain" />
+      <div class="flex flex-col">
+        <span class="text-md font-bold">{{ product().title }}</span><span class="text-md font-bold">{{ '$' + product().price }}</span>
+        <app-primary-button label="Add to Cart" class="mt-3" />
+      </div>
+    </div>
   `,
   styles: ``,
 })
 export class ProductCard {
-
+  product = input.required<Product>();
 }
