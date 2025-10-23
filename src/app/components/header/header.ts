cbb08d36262806b50b9b6fdeaa47525cb59ee27a
Luckey <luckeysherpa07@gmail.com>
2025-10-23 13:30:21 +0200

    removed one signal

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,14 +3,13 @@ import { Component, signal } from '@angular/core';
   selector: 'app-header',
   imports: [],
   template: `
-    <p class="bg-slate-100 px-4 py-3 shadow-md">
-      {{ title() }}
-    </p>
+    <div class="bg-slate-100 px-4 py-3 shadow-md">
+      <span>My Store</span>
+    </div>
   `,
   styles: `
 
   `,
 })
 export class Header {
-  title = signal('angular-ecommerce');
 }
