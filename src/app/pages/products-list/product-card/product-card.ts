a9a4a90aa139917330812be6dc9b11de0b29bda1
Luckey <luckeysherpa07@gmail.com>
2025-10-24 01:20:03 +0200

    added conditional styling

--- src/app/pages/products-list/product-card/product-card.ts
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -8,10 +8,18 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
   template: `
     <div class="bg-white shadow-md rounded-xl p-6 flex flex-col gap-6 relative">
       <div class="mx-auto">
-      <img [src]="product().image" class="w-[200px] h-[100px] object-contain" />
-      <div class="flex flex-col">
-        <span class="text-md font-bold">{{ product().title }}</span><span class="text-md font-bold">{{ '$' + product().price }}</span>
-        <app-primary-button label="Add to Cart" class="mt-3" />
+        <img [src]="product().image" class="w-[200px] h-[100px] object-contain" />
+        <div class="flex flex-col">
+          <span class="text-md font-bold">{{ product().title }}</span><span class="text-md font-bold">{{ '$' + product().price }}</span>
+          <app-primary-button label="Add to Cart" class="mt-3" />
+        </div>
+          <span class="absolute top-2 right-3 text-sm font-bold" [class]="product().stock ? 'text-green-600' : 'text-red-600'">
+            @if (product().stock) {
+              {{ product().stock }} left
+            } @else {
+              Out of Stock
+            }
+        </span>
       </div>
     </div>
   `,
