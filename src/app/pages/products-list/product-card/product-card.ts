70b944cd2db5e74e068b41f20d922ac521596587
Luckey <luckeysherpa07@gmail.com>
2025-10-24 01:49:07 +0200

    added logic for cart addition

--- src/app/pages/products-list/product-card/product-card.ts
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
 import { PrimaryButton } from "../../../components/primary-button/primary-button";
+import { Cart } from '../../../services/cart';
 
 @Component({
   selector: 'app-product-card',
@@ -11,7 +12,7 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
         <img [src]="product().image" class="w-[200px] h-[100px] object-contain" />
         <div class="flex flex-col">
           <span class="text-md font-bold">{{ product().title }}</span><span class="text-md font-bold">{{ '$' + product().price }}</span>
-          <app-primary-button label="Add to Cart" class="mt-3" />
+          <app-primary-button label="Add to Cart" class="mt-3" (btnClicked)="cartService.addToCart(product())" />
         </div>
           <span class="absolute top-2 right-3 text-sm font-bold" [class]="product().stock ? 'text-green-600' : 'text-red-600'">
             @if (product().stock) {
@@ -26,5 +27,7 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
   styles: ``,
 })
 export class ProductCard {
+
+  cartService = inject(Cart);
   product = input.required<Product>();
 }
