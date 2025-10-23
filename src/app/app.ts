316f64b1fa5813c899a8d8ac3d1b342a8ec909ca
Luckey <luckeysherpa07@gmail.com>
2025-10-23 13:05:29 +0200

    added header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, Header],
+  template: `<app-header/>`,
   styles: [],
 })
 export class App {
