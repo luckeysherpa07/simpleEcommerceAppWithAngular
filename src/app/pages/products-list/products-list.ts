24eb0e3d7a1910cb7cdaaada60b78bbecd8c1b94
Luckey <luckeysherpa07@gmail.com>
2025-10-24 00:58:46 +0200

    added product card

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Product } from '../../models/products.model';
+import { ProductCard } from './product-card/product-card';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [ProductCard],
   template: `
     <div class="p-8 grid grid-cols-2 gap-4">
       @for (product of products(); track product.id) {
-        {{product.title}}
+        <app-product-card />
       }
     </div>
   `,
