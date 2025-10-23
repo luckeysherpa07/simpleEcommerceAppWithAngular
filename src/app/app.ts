2a0749fdbbf52896895e68167f87ac56df82f2e2
Luckey <luckeysherpa07@gmail.com>
2025-10-24 00:49:17 +0200

    added product list in the main page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { ProductsList } from './pages/products-list/products-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
-  template: `<app-header/>`,
+  imports: [RouterOutlet, Header, ProductsList],
+  template: `
+    <app-header/>
+    <app-products-list />
+  `,
   styles: [],
 })
 export class App {
